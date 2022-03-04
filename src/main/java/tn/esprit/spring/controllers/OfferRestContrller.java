b5933965f2cd30b1c20a030a0b225d1e7357a2fd
aymenzaabar <aymen.zaabar@esprit.tn>
2022-03-05 00:49:01 +0100

    FilterSearch

--- src/main/java/tn/esprit/spring/controllers/OfferRestContrller.java
+++ src/main/java/tn/esprit/spring/controllers/OfferRestContrller.java
@@ -3,6 +3,8 @@ package tn.esprit.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
 import tn.esprit.spring.entities.Offer;
+import tn.esprit.spring.repository.IOfferRepository;
 import tn.esprit.spring.serviceInterface.offer.IOfferService;
 
 @RestController
 public class OfferRestContrller {
 	@Autowired
 	IOfferService OfferService;
+	@Autowired
+	IOfferRepository OfferRepo ;
+	
 	@ApiOperation(value = "Récupérer la liste des Offres")
 	@GetMapping("/retrieve-all-Offers")
 	@ResponseBody
@@ -53,5 +60,20 @@ public class OfferRestContrller {
 	public void updateOffer ( @RequestBody Offer offer) {
 		 		OfferService.updateOffer(offer);
 	}
+	
+	
+	/*@GetMapping("/list/{keyword}")
+	@ResponseBody
+    public void viewHomePage( @PathVariable(value="keyword") String keyword) {
+        List<Offer> listOffers = OfferService.listAll(keyword);
+        
+    }*/
+	
+	@GetMapping("/")
+    public List <Offer> FilterSearch( @Param("keyword") String keyword) {
+        return OfferService.listAll(keyword);
+      
+    }
+     
 
 }
