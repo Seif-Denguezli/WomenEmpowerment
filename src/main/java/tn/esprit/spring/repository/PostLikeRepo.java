1fabc863b54966ed509f107178d1f6858fed4b84
sabriKrima <sabri.krima@esprit.tn>
2022-03-08 13:51:02 +0100

    New function

--- src/main/java/tn/esprit/spring/repository/PostLikeRepo.java
+++ src/main/java/tn/esprit/spring/repository/PostLikeRepo.java
@@ -1,6 +1,10 @@
 package tn.esprit.spring.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +12,7 @@ import tn.esprit.spring.entities.*;
 @Repository
 public interface PostLikeRepo extends JpaRepository<PostLike, Long>{
 
+	
+	@Query(value =" SELECT * from users u  INNER JOIN post_like p ON p.user_user_id = u.user_id ORDER BY count(*)",nativeQuery=true)
+			public Set<Object> USer_order_by_Like ();
 }
