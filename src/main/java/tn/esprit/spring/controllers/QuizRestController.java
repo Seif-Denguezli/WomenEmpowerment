0cba640001a5cad4606649d93179a90a3d58f244
hassanhochlef <hassan.hochlef@esprit.tn>
2022-03-26 11:38:31 +0100

    Certificate model set

--- src/main/java/tn/esprit/spring/controllers/QuizRestController.java
+++ src/main/java/tn/esprit/spring/controllers/QuizRestController.java
@@ -127,13 +127,29 @@ public class QuizRestController {
 			.header("content-type", "application/json")
 			.header("X-RapidAPI-Host", "yakpdf.p.rapidapi.com")
 			.header("X-RapidAPI-Key", "b648c42070msh2f1e24111397e42p1155f4jsn864d7705eee5")
-			.method("POST", HttpRequest.BodyPublishers.ofString("{\r\n    \"pdf\": {\r\n        \"format\": \"A4\",\r\n        \"printBackground\": true,\r\n        \"scale\": 1\r\n    },\r\n    \"source\": {\r\n        \"html\": \"<!DOCTYPE html><html lang=\\\"en\\\"><head><meta charset=\\\"UTF-8\\\"><meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\"></head><body><div><center><h2>course certificate name</h2></center></br><center><h4>"+certif.getUser().getName()+" has earned this certificate</h4><h5>while completing the training course entitled "+certif.getCourse().getCourseName()+"</h5></center><h6>Courses area departement</h6><h6 >Courses management awarding team</h6><h6>Aquired "+certif.getObtainingDate()+"</h6><img src=\\\""+certif.getCertificateQR()+"\\\"></body></html>\"\r\n    },\r\n    \"wait\": {\r\n        \"for\": \"navigation\",\r\n        \"timeout\": 250,\r\n        \"waitUntil\": \"load\"\r\n    }\r\n}"))
+			.method("POST", HttpRequest.BodyPublishers.ofString("{\r\n    \"pdf\": {\r\n        \"format\": \"A4\",\r\n        \"printBackground\": true,\r\n        \"scale\": 1\r\n    },\r\n    \"source\": {\r\n        \"html\": \"<!DOCTYPE html><html lang=\\\"en\\\"><head><meta charset=\\\"UTF-8\\\"><meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\"></head><body><div style=\\\"width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #DB7093\\\"><div style=\\\"width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #FFC0CB\\\"><span style=\\\"font-size:50px; font-weight:bold\\\">Certificate of Completion</span><br><br><span style=\\\"font-size:25px\\\"><i>This is to certify that</i></span><br><br><span style=\\\"font-size:30px\\\"><b>"+certif.getUser().getName()+"</b></span><br/><br/><span style=\\\"font-size:25px\\\"><i>has completed the course</i></span> <br/><br/><span style=\\\"font-size:30px\\\"> "+certif.getCourse().getCourseName()+"</span> <br/><br/><br/><br/><br/><br/><span style=\\\"font-size:25px\\\"><i>For "+certif.getCourse().getNbHours()+"hours length</i></span><br><span style=\\\"font-size:25px;float:left\\\">Aquired on : "+certif.getObtainingDate()+"</span><div style=\\\"float:right\\\"><img src=\\\""+certif.getCertificateQR()+"\\\"></div></div></div></body></html>\"\r\n    },\r\n    \"wait\": {\r\n        \"for\": \"navigation\",\r\n        \"timeout\": 250,\r\n        \"waitUntil\": \"load\"\r\n    }\r\n}"))
 			.build();
 	 HttpResponse<byte[]> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofByteArray());
 	 byte[] res = response.body();
 	 return ResponseEntity.ok()
-             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=ordeesr.pdf") 
+             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=Certificate.pdf") 
              .contentType(MediaType.APPLICATION_PDF).body(res)
            ;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
