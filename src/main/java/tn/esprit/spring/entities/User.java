b9a6df61e4064e82bb44113647cc1d8407bc9215
sabriKrima <sabri.krima@esprit.tn>
2022-03-26 23:50:48 +0100

    chat

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -192,6 +192,12 @@ public class User implements Serializable{
 	
 	//******************************************************************//
 
-
+	// message
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="sender")
+	private Set<Message> senders;
+	
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="reciver")
+	private Set<Message> recivers;
 	
 	}
