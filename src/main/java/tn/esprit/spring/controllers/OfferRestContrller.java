f201a87adef7be6035dd2ee415c0a83f572f851d
aymenzaabar <aymen.zaabar@esprit.tn>
2022-03-29 00:03:07 +0100

    fix ambiguious

--- src/main/java/tn/esprit/spring/controllers/OfferRestContrller.java
+++ src/main/java/tn/esprit/spring/controllers/OfferRestContrller.java
@@ -3,27 +3,28 @@ package tn.esprit.spring.controllers;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 
 import javax.mail.MessagingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nylas.RequestFailedException;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import tn.esprit.spring.entities.Candidacy;
 import tn.esprit.spring.entities.Offer;
@@ -34,6 +35,7 @@ import tn.esprit.spring.serviceInterface.offer.IOfferService;
 
 @RestController
 public class OfferRestContrller {
+	
 	@Autowired
 	IOfferService OfferService;
 	@Autowired
@@ -42,6 +44,7 @@ public class OfferRestContrller {
 	ICandidacyService CandidacyService;
 	@Autowired
 	CalendarServiceImpl userAccount;
+	
 	@ApiOperation(value = "Récupérer la liste des Offres")
 	@GetMapping("/retrieve-all-Offers")
 	@ResponseBody
@@ -100,6 +103,19 @@ public class OfferRestContrller {
 	void ApplyOffer(@RequestBody Candidacy candidacy,@PathVariable("userId") Long userId,@PathVariable("offerId") Long offerId) {
 		CandidacyService.postulerOffre(candidacy, offerId, userId);
 	}
+	/*
+	@PostMapping("/upload/{candidacyId}")
+	  public ResponseEntity<ResponseMessage> uploadCv(@RequestPart("cv") MultipartFile cv,@PathVariable("candidacyId")Long candidacyId) {
+	    String message = "";
+	    try {
+	      storageService.store(cv,candidacyId);
+	      message = "Uploaded the file successfully: " + cv.getOriginalFilename();
+	      return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+	    } catch (Exception e) {
+	      message = "Could not upload the Cv: " + cv.getOriginalFilename() + "!";
+	      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+	    }
+	  }*/
 	
 	@PutMapping ("/Set-Favorite/{id}")
 	@ResponseBody
@@ -109,12 +125,12 @@ public class OfferRestContrller {
 	
 	
 	@GetMapping("/listMyCandidacy")
-    public List <String>  listMyCandidacy( @Param("keyword") String keyword) {
+    public List <String>  listMyCandidacy( @Param("userId") String keyword) {
         return CandidacyService.getMyCandidacy(keyword);
 	}
 	
 	@GetMapping("/listMyFavoriteCandidacy")
-    public List <String>  listMyFavoriteCandidacy( @Param("keyword") String keyword) {
+    public List <String>  listMyFavoriteCandidacy( @Param("userId") String keyword) {
         return CandidacyService.getMyFavoriteCandidacy(keyword);
 	}
 	
